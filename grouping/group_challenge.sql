152271b63d6b51f5b5a1ab21cb25621eb11eff4b
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-04-28 02:45:50 +0300

    GROUP Challenges

--- grouping/group_challenge.sql
+++ grouping/group_challenge.sql
@@ -2,7 +2,7 @@
 
 -- #1
 -- We have two staff members, with Staff IDs 1 and 2. 
--- We want to give abonus to the staff member that
+-- We want to give a bonus to the staff member that
 -- handled the most payments. (Most in terms of
 -- number of payments processed, not total
 -- dollar mount)
@@ -17,3 +17,31 @@ FROM payment
 GROUP BY staff_id;
 
 -- #2
+-- Corporate HQ conducting a study on the relationship between
+-- replacement cost and a movie MPAA rating (e.g. G,PG,R,etc).
+
+-- What's the average replacement cost per MPAA rating?
+-- NOTE: You may need to expand the AVG column to view correct result.
+
+SELECT * FROM film;
+
+SELECT rating, ROUND(AVG(replacement_cost),2)
+FROM film
+GROUP BY rating;
+
+SELECT rating, AVG(replacement_cost)
+FROM film
+GROUP BY rating
+ORDER BY AVG(replacement_cost) DESC;
+
+-- #3
+-- We are running a promotion to reward our top 5 customers with coupons.
+-- What are the customer ids of the top 5 customers by total spend?
+
+SELECT * FROM payment;
+
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
