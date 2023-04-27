868c8afe0aeec659f4c1153af7bb928a2b2ff5f5
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-04-27 02:45:16 +0200

    SQL Grouping Section -AGGREGATTE FUNCTION

---
+++ grouping/grouping_aggr_func_overview.sql
@@ -0,0 +1,48 @@
+-- GROUP BY
+-- How data is distributed per category.
+-- => Aggregate functions
+-- => Theory
+-- => Implementation
+-- => Having - Filltering with a GROUP BY
+-- => Challenges 
+
+-- Aggregate Functions
+-- Main idea is to take MULTIPLE inputs and return a SINGLE output.
+-- Most common aggr func.
+-- # AVG() - returns average(mean) value. 
+-- # COUNT() - returns number of values.
+-- # MAX() - returns maximum value. 
+-- # MIN() - returns minimum value. 
+-- # SUM() - returns the sum of all values. 
+
+-- NOTES: 
+-- Aggr func calls happen only in the SELECT clause or the HAVING clause.
+-- AVG func returns a floating point value (e.g. 2.343564...) Use ROUND().
+
+SELECT MIN(replacement_cost)
+FROM film;
+
+SELECT MAX(replacement_cost)
+FROM film;
+
+-- Can't use column name with aggregate method becuase it return 
+-- a single value but column may be return multiple.
+
+SELECT MAX(replacement_cost), MIN(replacement_cost)
+FROM film;
+
+SELECT MAX(replacement_cost), MIN(replacement_cost), COUNT(*)
+FROM film;
+
+SELECT AVG(replacement_cost)
+FROM film;
+
+SELECT ROUND(AVG(replacement_cost))
+FROM film;
+
+SELECT ROUND(AVG(replacement_cost),2) 
+FROM film;
+
+SELECT SUM(replacement_cost)
+FROM film;
+
