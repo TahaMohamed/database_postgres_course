707c1b13712e7be16958cd07c358dc0ba3926423
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-05-06 20:15:48 +0300

    Add 5 th Section

---
+++ 5th_section/data_types.sql
@@ -0,0 +1,15 @@
+-- Data Types
+
+-- Common Types
+-- Boolean (True, False)
+-- Char(char, varchar, text)
+-- Numeric(integer, floating-point number)
+-- Temporal(date, time, timestamp, interval)
+
+-- Uncommon Types
+-- UUID(Universally Unique Identifiers).
+-- Array(Stores an array of strings, numbers, etc.).
+-- JSON.
+-- Hstore key-value pair.
+-- Special types such as network address and geometric data.
+
